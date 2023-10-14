18f2534fa57b07d2f0a217475c697ed7b7ead846
minhqvu <thaiminhlam2013@gmail.com>
2023-10-14 16:55:20 -0700

    Ur mom

--- src/Buildings/BuildingPicker.java
+++ src/Buildings/BuildingPicker.java
@@ -1,7 +1,35 @@
 package Buildings;
 import java.util.*;
+import java.io.*;
 public class BuildingPicker {
     public Building target;
+
+    public Building[] createBuildings() throws FileNotFoundException{
+        Building[] result = new Building[28];
+        Scanner fileScan = new Scanner(new File("src\\Buildings\\buildings"));
+        int tracker = 0;
+        while(fileScan.hasNextLine()){
+            String buildingInfo = fileScan.nextLine();
+            // build out the info by seperating by commas
+            String abrev = buildingInfo.substring(0, buildingInfo.indexOf(","));
+            buildingInfo = buildingInfo.substring(abrev.length() + 1);
+
+            String fullName = buildingInfo.substring(0, buildingInfo.indexOf(","));
+            buildingInfo = buildingInfo.substring(fullName.length() + 1);
+
+            String tempLogi = buildingInfo.substring(0, buildingInfo.indexOf(","));
+            buildingInfo = buildingInfo.substring(tempLogi.length() + 1);
+
+            String tempLongi = buildingInfo.substring(0);
+
+            double logi = Double.parseDouble(tempLogi);
+            double longi = Double.parseDouble(tempLongi);
+            result[tracker] = new Building(abrev, fullName, logi, longi);
+            tracker++;
+        }
+
+        return result;
+    }
     public void buildingChoice(Building[] arr) {
         Random rand = new Random();
         int ind = rand.nextInt(28);
@@ -13,6 +41,7 @@ public class BuildingPicker {
         private String name;
         private double lat;
         private double longi;
+
         public Building (String abrev, String name, double lat, double longi) {
             this.abrev = abrev;
             this.name = name;
