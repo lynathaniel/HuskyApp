20ba381a804dd9a20fa504edee431a23e77630d0
minhqvu <thaiminhlam2013@gmail.com>
2023-10-14 16:39:28 -0700

    Ur mom

--- src/Buildings/BuildingPicker.java
+++ src/Buildings/BuildingPicker.java
@@ -1,31 +1,33 @@
 package Buildings;
-
 import java.util.*;
 public class BuildingPicker {
-
-
-
-public class Building {
-    private String abrev;
-    private String name;
-    private double lat;
-    private double longi;
-    public building (String abrev, String name, double lat, double longi) {
-        this.abrev = abrev;
-        this.name = name;
-        this.lat = lat;
-        this.longi = longi;
+    public Building target;
+    public void buildingChoice(Building[] arr) {
+        Random rand = new Random();
+        int ind = rand.nextInt(28);
+        this.target = arr[ind];
     }
 
-    public String getAbrev () {
-        return abrev;
-    }
 
-    public String getName () {
-        return name;
-    }
-
-    public double getLat () {
-        return lat;
+    public static class Building {
+        private String abrev;
+        private String name;
+        private double lat;
+        private double longi;
+        public building(String abrev, String name, double lat, double longi) {
+            this.abrev = abrev;
+            this.name = name;
+            this.lat = lat;
+            this.longi = longi;
+        }
+        public String getAbrev() {
+            return abrev;
+        }
+        public String getName() {
+            return name;
+        }
+        public double getLat() {
+            return lat;
+        }
     }
 }
