45e8cfb5d08c6e3634fec1dcf258db9ca6aed00d
Andororo <andororoo@gmail.com>
2023-10-15 05:09:12 -0700

    init

--- huskyapp/src/main.jsx
+++ huskyapp/src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import StartUp from './StartUp.jsx'
-
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <StartUp />
+    <App />
   </React.StrictMode>,
 )
