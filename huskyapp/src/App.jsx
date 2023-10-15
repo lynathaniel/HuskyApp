627f46e2c3eb6213e228e507389a3d83a446559d
RickyLe01 <RickyDangLe01@gmail.com>
2023-10-14 22:07:53 -0700

    Working on Front Page

--- huskyapp/src/App.jsx
+++ huskyapp/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
+import { Header } from './Header.jsx';
+import { History } from './Nav.jsx';
+//import { ChatPane } from './ChatPane.js';
 function App() {
-  const [count, setCount] = useState(0)
 
+  //what content should my App look like?
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <Header />
+      <div className="row flex-grow-1">
+        <div>
+          <History />
+        </div>
+        <div>
+          {/* <ChatPane /> */}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
+
 
 export default App
