724932ef284e60d20ca62de987313fb1e79054e9
Andororo <61718459+Andororo@users.noreply.github.com>
2023-10-15 05:57:29 -0700

    Create GeoLocator.jsx

---
+++ HuskyApp/src/components/GeoLocator.jsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+import VideoCapture from "./VideoCapture";
+
+function GeoLocator() {
+
+  let id;
+  let target;
+  let options;
+  let abrev;
+  let buildingName;
+  let lng;
+  let lat;
+  let found = false;
+
+  const [Listing, setListing] = useState([]);
+
+  const num = Math.floor(Math.random() * 4) + 1
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://127.0.0.1:8000/api/todos/" + num + "/");
+      const json = await response.json();
+      setListing(json);
+    };
+
+    fetchData();
+  }, []);
+  
+  abrev = Listing.abrev
+  buildingName = Listing.fullname
+  lng = parseFloat(Listing.longitude)
+  lat = parseFloat(Listing.latitude)
+  
+
+  console.log(lat)
+
+
+  function success(pos) {
+    const crd = pos.coords;
+    if (crd.longitude < lng + 10.0 &&
+        crd.longitude > lng - 10.0 &&
+        crd.latitude < lat + 10.0 &&
+        crd.latitude > lat - 10.0) {
+          alert("You found " + buildingName + "!")
+          found = true
+        } else {
+          alert("Keep looking around campus for the building of the day!")
+        }
+    navigator.geolocation.clearWatch(id)
+  }
+  
+  function error(err) {
+    console.error(`ERROR(${err.code}): ${err.message}`);
+  }
+  
+  target = {
+    latitude: 0,
+    longitude: 0,
+  };
+  
+  options = {
+    enableHighAccuracy: false,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+  
+  function getLoc() { id = navigator.geolocation.getCurrentPosition(
+    success,
+    error,
+    // maximumAge refers to the age of the location data in cache,
+    // setting it to infinity guarantees you'll get a cached version.
+    // Setting it to 0 forces the device to retrieve the position.
+    // http://stackoverflow.com/questions/3397585/navigator-geolocation-getcurrentposition-sometimes-works-sometimes-doesnt
+    { maximumAge: 15000,
+      timeout: 30000,
+      enableHighAccuracy: false
+    }
+  );
+  }
+
+  return (
+    <div>
+      <button className="cover" onClick={() => getLoc()}></button>
+      <div className="video">
+      <VideoCapture isVisible={found}/>
+      </div>
+    </div>
+  );
+
+}
+
+export default GeoLocator;
